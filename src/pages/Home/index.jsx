d6012b40ba88919746e2e9c9f7210d1958d5dc67
IvanildoBorges <ivanildo.rap@gmail.com>
2025-05-15 20:35:42 -0300

    Adicionado seção de banner responsivo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,79 @@
-import {
-    Section,
-    Subtitle,
-    Title
-} from "./style";
+import { Carousel } from 'primereact/carousel';
+import { useEffect, useState } from "react";
+import ornamento from "../../assets/Ornament.svg";
+import { Section } from "./style";
+import destaqueIMG from "/White-Sneakers.png";
 
 const Home = () => {
+    const [banners, setBanners] = useState([]);
+    const api = {
+        getBanners: async () => {
+            return [
+                {
+                    impacto: "Melhores ofertas personalizadas",
+                    titulo: "Queima de stoque Nike 🔥",
+                    subtitulo: "Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+                    imagem: destaqueIMG
+                },
+                {
+                    impacto: "Melhores ofertas personalizadas",
+                    titulo: "Queima de stoque Nike 🔥",
+                    subtitulo: "Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+                    imagem: destaqueIMG
+                },
+                {
+                    impacto: "Melhores ofertas personalizadas",
+                    titulo: "Queima de stoque Nike 🔥",
+                    subtitulo: "Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+                    imagem: destaqueIMG
+                },
+                {
+                    impacto: "Melhores ofertas personalizadas",
+                    titulo: "Queima de stoque Nike 🔥",
+                    subtitulo: "Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+                    imagem: destaqueIMG,
+                    textoAlternativo: "Produto destaque"
+                }
+            ]
+        },
+    };
+
+    useEffect(() => {
+        api.getBanners().then((data) => setBanners(data));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const bannerTemplate = (banner) => {
+        return (
+            <div className="banner__container">
+                <div className="left__content">
+                    <p className="impact__text">{banner.impacto}</p>
+                    <h1>{banner.titulo}</h1>
+                    <p className="subtitle">{banner.subtitulo}</p>
+                    <button className="primary-button">Ver Ofertas</button>
+                </div>
+                <div className="right__content">
+                    <img src={banner.imagem} alt={banner.textoAlternativo} />
+                </div>
+            </div>
+        );
+    };
+    
     return ( 
-        <Section className="beggin">
-            <Title>Queima de estoque Nike</Title>
-            <Subtitle>A project react with typescript.</Subtitle>
-        </Section>
+        <>
+            <Section className="banner">
+                <Carousel 
+                    value={banners} 
+                    numVisible={1} 
+                    numScroll={1} 
+                    className="custom-carousel" 
+                    circular
+                    autoplayInterval={3000} 
+                    itemTemplate={bannerTemplate} 
+                />
+                <img className="img-absolute" src={ornamento} alt="Ornamento" />
+            </Section>
+        </>
      );
 }
  
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle as globalStyle } from "styled-components";
-import { actionsColors } from "./colors/cores";
+import { actionsColors, grayScaleColors } from "./colors/cores";
 import { tipography } from "./fonts/tipografia";
 
 const Global = globalStyle`
@@ -16,6 +16,7 @@ const Global = globalStyle`
    body, input, textarea {
         font-family: ${tipography.familia};
         font-weight: ${tipography.peso};
+        color: ${grayScaleColors.darkGray2};
     }
 
     body {
@@ -23,11 +24,34 @@ const Global = globalStyle`
         min-width: 320px;
         min-height: 100dvh;
         background-color: ${actionsColors.bg};
+        font-size: 1rem;
         font-synthesis: none;
         text-rendering: optimizeLegibility;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         -webkit-text-size-adjust: 100%;
+    }
+
+    button, input {
+        border: none;
+    }
+
+    .primary-button {
+        font-size: 1rem;
+        font-weight: 700;
+        line-height: 24px;
+        letter-spacing: .75px;
+        font-family: ${tipography.familia};
+        background-color: ${actionsColors.primary};
+        color: ${grayScaleColors.lightGray3};
+        padding: .75rem 1rem;
+        border-radius: .5rem;
+        cursor: pointer;
+        transition: all .3s ease-in;
+    }
+
+    .primary-button:hover {
+        background-color: ${actionsColors.tertiary};
     }
 `;
 
